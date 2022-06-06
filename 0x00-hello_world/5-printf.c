f5cb8ca4d9dcf5c7b87137b103f0da9674e52b98
christian <christianudoh84@gmail.com>
2022-06-06 15:13:28 -0700

    another commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,15 +2,13 @@
 
 /**
  *  *   * main - Entry point
- *   *   *  
- *    *   *Return: Always 0 (success)
+ *   *    *  
+ *    *    *Return: Always 0 (success)
  *     **/
-
 int main(void)
-
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art");
 
 	return (0);
 
