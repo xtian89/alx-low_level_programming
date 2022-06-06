28cc218c31666aaf962215364de22976e0d81fba
christian <christianudoh84@gmail.com>
2022-06-06 14:47:08 -0700

    another commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ *  *  * main - Prints out a string to stdout.
+ *   *   *
+ *    *   *Return: Always 0 (success)
+ *     **/
 
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
