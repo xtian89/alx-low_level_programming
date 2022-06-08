5f9b3adfb0fa0a7a55657c192146e56902b36bab
christian <christianudoh84@gmail.com>
2022-06-08 14:22:41 -0700

    another fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-*main - prints putchar
-*Return: always 0
+*	main - prints putchar
+*	Return: always 0
 */
 int main(void)
 {
@@ -14,7 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
+	_putchar('\n');	
 
 	return (0);
 
