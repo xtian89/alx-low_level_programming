5aaf15471e5d5b48789543502544d98fb30dcb3d
christian <christianudoh84@gmail.com>
2022-06-08 14:03:40 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - prints putchar
+ * Return: always 0
+*/
 int main(void)
-
 {
+	_putchar('_');
+        _putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-		char *sh = "Holberton";
-
-
-
-			while (*sh)
-
-					{
-
-								_putchar(*sh);
-
-										sh++;
-
-											}
-
-				_putchar('\n');
-
-
-
-					return (0);
+	return (0);
 
 }
