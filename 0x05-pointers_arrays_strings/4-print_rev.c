76fcd5c1dd7a8a7bcbf653a5b1ed82244d590857
christian <christianudoh84@gmail.com>
2022-06-13 14:52:30 -0700

    Initial

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev -> prints a string, in reverse
+ * fcounter -> count to end, and count back
+ * Return: string in reverse
+ */
+void print_rev(char *s)
+{
+	int fcounter = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		fcounter++;
+	}
+	
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
