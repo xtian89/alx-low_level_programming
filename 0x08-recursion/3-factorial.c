18b449c4fd5aa192ad733eed44742cf101b3ff3c
christian <christianudoh84@gmail.com>
2022-06-23 03:59:55 -0700

    Initial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial -> prints the factorial of a number
+ * @n: Integer
+ * Return: on success 1
+ */
+
+int factorial(int n)
+{
+	if(n == 0)
+		return 1;
+	else
+		return n * factorial(n - 1);
+}
