0ce708338da6a470b4b1fe021eb8fa4470ab5855
christian <christianudoh84@gmail.com>
2022-07-04 08:47:41 -0700

    Initial

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#ifndef _FILE_
+#define _FILE_
+
+#endif
+
+/**
+ * main - print the name of the file
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
